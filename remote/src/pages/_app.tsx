b0918ab0bbd832b3cdf0b03dad8d75c547f4f93c
Joey Gough <j.gough@novafutur.com>
2023-04-18 12:46:44 +0100

    init projects

---
+++ remote/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@remote/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
